db9915af130b6c59c8df402dbf7a2deef1308c22
Andrey Kats <andreykats@gmail.com>
2024-04-03 15:37:34 -0400

    config

--- Adafruit_Video_Looper/video_looper.py
+++ Adafruit_Video_Looper/video_looper.py
@@ -62,9 +62,9 @@ class VideoLooper:
         self._keyboard_control = self._config.getboolean('control', 'keyboard_control')
         self._copyloader = self._config.getboolean('copymode', 'copyloader')
         # Get seconds for countdown from config
-        self._countdown_time = self._config.getint('video_looper', 'countdown_time')
+        self._countdown_time = 3
         # Get seconds for waittime bewteen files from config
-        self._wait_time = 3
+        self._wait_time = self._config.getint('video_looper', 'wait_time')
         # Get timedisplay settings
         self._datetime_display = self._config.getboolean('video_looper', 'datetime_display')
         self._top_datetime_display_format = self._config.get('video_looper', 'top_datetime_display_format', raw=True)
