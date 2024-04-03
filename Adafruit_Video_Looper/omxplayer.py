06eabf312be9f140a15eb9628f9e27f005b4af03
Andrey Kats <andreykats@gmail.com>
2024-04-03 17:46:05 -0400

    debug

--- Adafruit_Video_Looper/omxplayer.py
+++ Adafruit_Video_Looper/omxplayer.py
@@ -61,8 +61,7 @@ class OMXPlayer:
         # Assemble list of arguments.
         args = ['omxplayer']
         args.extend(['-o', self._sound])  # Add sound arguments.
-        print("time: " + self.get_elapsed_time())
-        args.extend(['-l', "00:07:00"])  # Add starting position.
+        args.extend(['-l', self.get_elapsed_time()])  # Add starting position.
         args.extend(self._extra_args)     # Add extra arguments from config.
         if vol != 0:
             args.extend(['--vol', str(vol)])
