f3bbd4e49562f19a86e5b4f61a304f4de91d3f41
Andrey Kats <andreykats@gmail.com>
2026-01-19 18:40:04 -0500

    Enable video stretch to fill screen for 4:3 videos

--- Adafruit_Video_Looper/mpv.py
+++ Adafruit_Video_Looper/mpv.py
@@ -221,7 +221,7 @@ class MPVPlayer:
 
         # Video stretch - ignore aspect ratio to fill screen (no black bars)
         if self._video_stretch:
-            args.extend(['--video-aspect-override=4:3'])
+            args.extend(['--no-keepaspect'])
 
         # Handle DRM connector if specified
         if self._drm_connector:
